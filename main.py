6c464f5029cfaa6dffd014fabcf15bded6c0e1f8
Erel shtossel <erel.shtossel@live.biu.ac.il>
2021-03-08 18:13:23 +0200

    update test data

--- main.py
+++ main.py
@@ -10,31 +10,24 @@ from models import siggraph17
 from train_loop import train, back_to_color
 
 if __name__ == '__main__':
-    if torch.cuda.is_available():
-        dataset_gt = wds.WebDataset("Line_Drawing_Colorization_Followup/GT.tar").decode("rgb8").decode(
-            my_decoder_GT).to_tuple("jpg;png", "__key__")
-        dataset_td = wds.WebDataset("Line_Drawing_Colorization_Followup/train_data.tar").decode("rgb8").decode(
-            my_decoder_BW).to_tuple("jpg;png", "__key__")
-        dataset = SampleEqually([dataset_gt, dataset_td])
-    else:
-        dataset_gt = wds.WebDataset("GT.tar").decode("rgb8").decode(
-            my_decoder_GT).to_tuple("jpg;png", "__key__")
-        dataset_td = wds.WebDataset("train_data.tar").decode("rgb8").decode(
-            my_decoder_BW).to_tuple("jpg;png", "__key__")
-        dataset = SampleEqually([dataset_gt, dataset_td])
-    dataloader = torch.utils.data.DataLoader(dataset, num_workers=4, batch_size=16, collate_fn=collate,
+    dataset_gt = wds.WebDataset("GT_train.tar").decode("rgb8").decode(
+        my_decoder_GT).to_tuple("jpg;png", "__key__")
+    dataset_td = wds.WebDataset("train_data_train.tar").decode("rgb8").decode(
+        my_decoder_BW).to_tuple("jpg;png", "__key__")
+    dataset = SampleEqually([dataset_gt, dataset_td])
+    dataloader = torch.utils.data.DataLoader(dataset, num_workers=4, batch_size=64, collate_fn=collate,
                                              prefetch_factor=1)
 
     # TODO:spit train and test
     # createClassMatrix()
     # gatherClassImbalanceInfo(dataloader)
 
-    # model = siggraph17(pretrained_path=None)
-    # model = train(dataloader, model)
+    model = siggraph17(pretrained_path=None)
+    model = train(dataloader, model)
 
-    model = siggraph17(pretrained_path="model_iter0")
-    dataset_td = wds.WebDataset("train_data.tar").decode("rgb8").decode(
-        my_decoder_BW).to_tuple("jpg;png", "__key__")
-    dataloader = torch.utils.data.DataLoader(dataset_td)
-    for i, (input_batch) in enumerate(dataloader):
-        back_to_color(model(torch.tensor(input_batch[0], dtype=torch.uint8)))
+    # model = siggraph17(pretrained_path="model_iter0")
+    # dataset_td = wds.WebDataset("train_data.tar").decode("rgb8").decode(
+    #     my_decoder_BW).to_tuple("jpg;png", "__key__")
+    # dataloader = torch.utils.data.DataLoader(dataset_td)
+    # for i, (input_batch) in enumerate(dataloader):
+    #     back_to_color(model(torch.tensor(input_batch[0], dtype=torch.uint8)))
