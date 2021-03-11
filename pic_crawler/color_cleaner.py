6ae7d3c037a096b539463bc8c58ca96de6938f89
Erel shtossel <erel.shtossel@live.biu.ac.il>
2021-03-11 10:29:03 +0200

    Add image crawler

---
+++ pic_crawler/color_cleaner.py
@@ -0,0 +1,185 @@
+import os
+from threading import Lock
+
+import cv2
+import numpy as np
+
+
+
+def show_image(pic):
+    cv2.imshow("p", pic)
+    cv2.waitKey(0)
+
+
+def distance_in_percentage(x, y):
+    if (x / y) >= 1:
+        return (x / y) - 1
+    else:
+        return (y / x) - 1
+
+
+def clean_white_background(pic):
+    # Convert to gray, and threshold
+    gray = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+    th, threshed = cv2.threshold(gray, 240, 255, cv2.THRESH_BINARY_INV)
+
+    # Morph-op to remove noise
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))
+    morphed = cv2.morphologyEx(threshed, cv2.MORPH_CLOSE, kernel)
+
+    # Find the max-area contour
+    cnts = cv2.findContours(morphed, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+    cnt = sorted(cnts, key=cv2.contourArea)[-1]
+
+    # Crop and and try to create square if possible
+    x, y, w, h = cv2.boundingRect(cnt)
+    if w < h:
+        d = int(min(h - w, x * 2) / 2)
+        x -= d
+        w += 2 * d
+    else:
+        d = int(min(w - h, y * 2) / 2)
+        y -= d
+        h += 2 * d
+    dst = pic[y:y + h, x:x + w]
+    return dst
+
+
+def resize_pic(pic: np.ndarray, res=256, squaring_threshold=0.1) -> list:
+    pic = clean_white_background(pic)
+    if min(pic.shape[0], pic.shape[1]) < 256:
+        # This image is too small for our needs
+        return []
+    pics = []
+    if pic.shape[0] == pic.shape[1]:
+        # Normal resize
+        resized_pic = cv2.resize(pic, (res, res), interpolation=cv2.INTER_AREA)
+        pics.append(resized_pic)
+    elif distance_in_percentage(pic.shape[0], pic.shape[1]) <= squaring_threshold:
+        # We resize in the normal way, even that the original image is not exactly squared
+        resized_pic = cv2.resize(pic, (res, res), interpolation=cv2.INTER_AREA)
+        pics.append(resized_pic)
+    elif distance_in_percentage(pic.shape[0], pic.shape[1]) <= squaring_threshold * 2:
+        # TODO: Maybe off scale a bit for more image coverage
+        # We don't want to window with this small overlap
+        scale_to = 256 / (min(pic.shape[0], pic.shape[1]))
+        resized_pic = cv2.resize(pic, dsize=(0, 0), fx=scale_to, fy=scale_to, interpolation=cv2.INTER_AREA)
+        # Crop the middle of the image
+        x = int((resized_pic.shape[0] / 2) - 0.5 * res)
+        y = int((resized_pic.shape[1] / 2) - 0.5 * res)
+        cropped_pic = resized_pic[x:x + res, y:y + res]
+        pics.append(cropped_pic)
+    else:
+        # We need to resize, and then create picture windows
+        scale_to = 256 / (min(pic.shape[0], pic.shape[1]))
+        resized_pic = cv2.resize(pic, dsize=(0, 0), fx=scale_to, fy=scale_to, interpolation=cv2.INTER_AREA)
+
+        # Crop 256*256 boxes
+        for y in range(0, resized_pic.shape[0], res):
+            for x in range(0, resized_pic.shape[1], res):
+                pic_window = resized_pic.copy()
+                boxed_pic = pic_window[x:x + res, y:y + res]
+                pics.append(boxed_pic)
+        # Now we want the last box in the proper size:
+        pics = pics[:-1]
+        pic_window = resized_pic.copy()
+        boxed_pic = pic_window[pic_window.shape[0] - res:, pic_window.shape[1] - res:]
+        pics.append(boxed_pic)
+
+    return pics
+
+
+def load_pic(pic_path):
+    pic = cv2.imread(pic_path)
+    return pic
+
+
+def remove_low_colored_pics(pics, bw_threshold=20):
+    colorful_pics = []
+    for pic in pics:
+        pic_hsv = cv2.cvtColor(pic, cv2.COLOR_BGR2HSV)
+        # is the picture colorful enough? Check the mean value of the saturation.
+        mean, std = cv2.meanStdDev(pic_hsv)
+        if mean[1] < bw_threshold:
+            continue
+        colorful_pics.append(pic)
+    return colorful_pics
+
+
+def linerize1(pics):
+    """
+    This is the way to binaries the pictures as introduced in the original paper
+    :param pics: The images to linearize
+    :return: Linearized pics
+    """
+    line_pics = []
+    neiborhood8 = np.ones((3, 3), dtype=np.uint8)
+    for pic in pics:
+        gray_form = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+        # img_dilate = cv2.erode(gray_form, neiborhood8, iterations=2)
+        img_dilate = cv2.dilate(gray_form, neiborhood8, iterations=1)
+
+        img_diff = cv2.absdiff(gray_form, img_dilate)
+        img_diff = cv2.multiply(img_diff, 3)
+        img_line = cv2.bitwise_not(img_diff)
+
+        # img_line = cv2.adaptiveThreshold(img_line, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 7, 8)
+        img_binary = cv2.adaptiveThreshold(img_line, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 21, 8)
+
+        line_pics.append(img_binary)
+    return line_pics
+
+
+def linerize(pics):
+    """
+    This is the way to binaries the pictures as introduced in the original paper
+    :param pics: The images to linearize
+    :return: Linearized pics
+    """
+    line_pics = []
+    neiborhood8 = np.ones((3, 3), dtype=np.uint8)
+    for pic in pics:
+        gray_form = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+        img_dilate = cv2.erode(gray_form, neiborhood8, iterations=2)
+        img_dilate = cv2.dilate(img_dilate, neiborhood8, iterations=4)
+
+        img_diff = cv2.absdiff(gray_form, img_dilate)
+        img_diff = cv2.multiply(img_diff, 3)
+        img_line = cv2.bitwise_not(img_diff)
+
+        kernel = np.ones((8, 8), np.float32) / 64
+        img_line = cv2.filter2D(img_line, -1, kernel)
+        img_line = cv2.dilate(img_line, neiborhood8, iterations=1)
+        img_binary = cv2.adaptiveThreshold(img_line, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 15, 8)
+        # _, img_binary = cv2.threshold(img_line, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        img_line_tmp = np.zeros_like(img_binary)
+        img_line_tmp[img_binary > 200] = 255
+        img_line = img_line_tmp
+        line_pics.append(img_line)
+    return line_pics
+
+
+def clean_color(pic_path, save_path) -> None:
+    """
+    Takes an image, checks that the resolution is good and resize it to 256 on at least one of the axes, then crops it
+    to 256X256 chunks and picks a chunk if it colorful enough. Then the chunk is cleaned from color, and saved in it's
+    colored form as ground truth (in save_path/GT/ folder) and the uncolored form as train data
+    (in save_path/train_data/ folder).
+    :param pic_path: The path of the original image
+    :param save_path: The path of GT and train data folders for saving the output
+    :return: None
+    """
+    pic = load_pic(pic_path)
+    resize_pics = resize_pic(pic)
+    only_colorful_pics = remove_low_colored_pics(resize_pics)
+
+    for i, (pic) in enumerate(only_colorful_pics):
+        gt_file_path = f"{save_path}/GT/GT_{pic_path[pic_path.rindex('/') + 1:pic_path.rindex('.')]}_{str(i)}.jpg"
+        cv2.imwrite(gt_file_path, pic)
+
+    lined_pics = linerize(only_colorful_pics)
+
+    for i, (pic) in enumerate(lined_pics):
+        train_file_path = f"{save_path}/train_data/train_{pic_path[pic_path.rindex('/') + 1:pic_path.rindex('.')]}_{str(i)}.jpg"
+        cv2.imwrite(train_file_path, pic)
---
+++ pic_crawler/utils_crawler.py
@@ -0,0 +1,129 @@
+import multiprocessing
+import os
+from argparse import ArgumentParser
+import tarfile
+import urllib
+from time import sleep
+
+import cv2
+import urllib3
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from posixpath import basename
+
+import threading
+import concurrent.futures
+
+from color_cleaner import clean_color
+
+max_thread = 10
+
+
+def downloadPic(url, folderName):
+    try:
+        response = urllib.request.urlopen(url, timeout=10)
+        soup = BeautifulSoup(response.read(), "html.parser")
+
+        picObj = soup.find('div', id='content')
+        linkObj = picObj.find('img')
+        imgUrl = urljoin(url, linkObj['src'])
+
+        outputName = '%s/%s.jpg' % (folderName, basename(url))
+        urllib.request.urlretrieve(imgUrl, outputName)
+        clean_color(outputName, folderName)
+        os.remove(outputName)
+        print("OK: " + url)
+    except urllib.error.HTTPError as err:
+        print("FAIL: " + url + " " + str(err))
+    except cv2.error as err:
+        print("FAIL: " + url + " " + str(err))
+        os.remove(outputName)
+    except Exception as exp:
+        print("FATAL FAIL: " + str(exp))
+
+
+def tarify(folderName):
+    all_gts = os.listdir(f"{folderName}/GT")
+    all_train_data = os.listdir(f"{folderName}/train_data")
+    if len(all_gts) == 0 or len(all_train_data) == 0:
+        return
+    if len(all_gts) > len(all_train_data):
+        all_gts = all_gts[:len(all_train_data)]
+    elif len(all_train_data) > len(all_gts):
+        all_train_data = all_train_data[:len(all_gts)]
+    take_only = len(all_gts) - len(all_gts) % 10
+    all_gts = all_gts[:take_only]
+    all_train_data = all_train_data[:take_only]
+    train_num = int(take_only * 0.8)
+
+    all_gts_train = all_gts[:train_num]
+    all_gts_test = all_gts[train_num:]
+    all_train_data_train = all_train_data[:train_num]
+    all_train_data_test = all_train_data[train_num:]
+
+    def to_tar(file_name, data_to_save):
+        tar_file = tarfile.open(folderName + "/" + file_name + ".tar", mode="a")
+        if "GT" in file_name:
+            folder = "GT"
+        else:
+            folder = "train_data"
+        for file in data_to_save:
+            tar_file.add(folderName + "/" + folder + "/" + file, file[file.rfind("\\") + 1:])
+            os.remove(folderName + "/" + folder + "/" + file)
+        tar_file.close()
+
+    for data, tar_file in zip([all_gts_train, all_gts_test, all_train_data_train, all_train_data_test],
+                              ["GT_train", "GT_test", "train_data_train", "train_data_test"]):
+        to_tar(tar_file, data)
+
+    return
+
+
+def scrape(base_url, folderName):
+    thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=max_thread)
+    ths = []
+    i = 0
+
+    for curr_img in reversed(range(0, 3192000)):
+        if os.path.isfile(os.path.join(folderName, '%d.jpg' % curr_img)):
+            continue
+        if i % 10 == 0:
+            sleep(1)
+            th = thread_pool.submit(tarify, folderName)
+            th.result()
+        # sema.acquire(True)
+        url = '%s%d' % (base_url, curr_img)
+        th = thread_pool.submit(downloadPic, url, folderName)
+        th.result()
+        # ths.append(th)
+        i += 1
+
+    # wait for the threads to finish
+    thread_pool.shutdown()
+
+
+def parseCLI():
+    desc = u'{0} [Args] [Options]\nDetailed options -h or --help'.format(__file__)
+
+    parser = ArgumentParser(description=desc)
+
+    parser.add_argument('-u', '--url', type=str, dest='url', required=True,
+                        help='URL of the website to crawl in')
+    parser.add_argument('-f', '--folderName', type=str, dest='folderName', required=True,
+                        help='Crawl output folder')
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parseCLI()
+
+    # make the directory for the created files
+    os.makedirs(args.folderName, exist_ok=True)
+    os.makedirs(args.folderName + "/GT", exist_ok=True)
+    os.makedirs(args.folderName + "/train_data", exist_ok=True)
+
+    scrape(args.url, args.folderName)
+
+    print("done")
