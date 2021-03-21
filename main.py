0b17d700b2c10af933ef52ec2caab767b9855d4d
Erel shtossel <erel.shtossel@live.biu.ac.il>
2021-03-21 19:42:29 +0200

    Clean code

--- main.py
+++ main.py
@@ -29,15 +29,17 @@ if __name__ == '__main__':
     #
     model = siggraph17_L(pretrained_path=None)
     if torch.cuda.is_available():
-        dataset = wds.WebDataset("train_{0000000..0000001}.tar", length=float("inf")) \
-            .decode(my_decoder_GT).decode(my_decoder_BW).to_tuple("gt.jpg", "train.jpg", "__key__").batched(6)
+        # dataset = wds.WebDataset("train_{0000000..0000001}.tar", length=float("inf")) \
+        #     .decode(my_decoder_GT).decode(my_decoder_BW).to_tuple("gt.jpg", "train.jpg", "__key__").batched(6)
+        dataset = wds.WebDataset("preprocessed_data_tars.tar", length=float("inf")) \
+            .map(tarfilter).to_tuple("gt.pt", "train.pt", "__key__").batched(4)
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=None, num_workers=2)
         trainer = pl.Trainer(gpus=1, log_every_n_steps=10, max_epochs=10, profiler=True,
                              max_steps=150, distributed_backend='ddp', precision=16)
     else:
         dataset = wds.WebDataset("preprocessed_data_tars.tar", length=float("inf")) \
             .map(tarfilter).to_tuple("gt.pt","train.pt","__key__").batched(2)
-
         dataloader = torch.utils.data.DataLoader(dataset, batch_size=None)
         trainer = pl.Trainer(log_every_n_steps=10, max_epochs=10, profiler=True, max_steps=5)
+
     trainer.fit(model, dataloader)
