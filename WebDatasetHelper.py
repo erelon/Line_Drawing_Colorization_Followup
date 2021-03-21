184cec52102df1b5c2883589a4931f5229898e5b
Erel shtossel <erel.shtossel@live.biu.ac.il>
2021-03-21 13:56:21 +0200

    try to util gpu to max

--- WebDatasetHelper.py
+++ WebDatasetHelper.py
@@ -87,7 +87,7 @@ def my_decoder_GT(key, data):
 
     im_GT = soft_encode_image(im_GT)
 
-    back_to_color(im_GT.unsqeeze(0))
+    back_to_color(im_GT.unsqueeze(0))
 
     if type(im_GT) is torch.Tensor:
         return im_GT
