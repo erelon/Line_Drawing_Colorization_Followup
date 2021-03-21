7726ddd17500ea0b41fbbeb86b9cfc1cc3c18191
Erel shtossel <erel.shtossel@live.biu.ac.il>
2021-03-21 13:31:11 +0200

    try to util gpu to max

--- CoreElements.py
+++ CoreElements.py
@@ -182,7 +182,7 @@ def rgb2lchTensor(rgb):
     # Nonlinear distortion and linear transformation
     mask = rgb > 0.008856
     rgb[mask] = torch.pow(rgb[mask], 1 / 3)
-    rgb = rgb.type(torch.float16).cuda()
+    # rgb = rgb.type(torch.float16).cuda()
     rgb[~mask] = 7.787 * rgb[~mask] + 16. / 116.
 
     x, y, z = rgb[..., 0], rgb[..., 1], rgb[..., 2]
@@ -196,7 +196,7 @@ def rgb2lchTensor(rgb):
 
     a, b = lab[..., 1], lab[..., 2]
     a, b = torch.hypot(a, b), torch.atan2(b, a)
-    b += torch.where(a < 0., 2 * np.pi, 0)
+    b += torch.where(a < 0., 2 * np.pi, 0.)
 
     lab[..., 1], lab[..., 2] = a, b
 
