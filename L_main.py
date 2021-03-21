b8330ce67e62cdd52170c036f5543f88f50d76e3
Erel shtossel <erel.shtossel@live.biu.ac.il>
2021-03-21 14:14:26 +0200

    try to util gpu to max

--- L_main.py
+++ L_main.py
@@ -35,7 +35,7 @@ if __name__ == '__main__':
     if torch.cuda.is_available():
         dataset = wds.WebDataset("train_{0000000..0000001}.tar", length=float("inf")) \
             .decode(my_decoder_GT).decode(my_decoder_BW).to_tuple("gt.jpg", "train.jpg", "__key__").batched(4)
-        dataloader = torch.utils.data.DataLoader(dataset, batch_size=None,num_workers=2)
+        dataloader = torch.utils.data.DataLoader(dataset, batch_size=None)
         trainer = pl.Trainer(gpus=1, log_every_n_steps=10, max_epochs=10, profiler=True,
                              max_steps=150, distributed_backend='ddp', precision=16)
     else:
